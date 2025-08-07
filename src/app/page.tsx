1352403d38ae80450904bc856e47a81c949d3911
brngxs <devbrenoserpa@gmail.com>
2025-08-07 13:09:26 -0300

    [FEAT]: adding product list components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,38 @@
 import { Header } from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import { db } from "@/db";
+import Image from "next/image";
 
-const Home = () => {
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
   return (
     <>
       <Header />
+      <div className="space-y-6 px-5">
+        <Image
+          src="/banner-01.png"
+          alt="Leve uma vida com estilo"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+
+        <ProductList products={products} title="Mais vendidos" />
+
+        <Image
+          src="/banner-02.png"
+          alt="Leve uma vida com estilo"
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto w-full"
+        />
+      </div>
     </>
   );
 };
